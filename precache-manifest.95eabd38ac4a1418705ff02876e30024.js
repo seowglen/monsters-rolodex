cbe7b073bc3446f25635c046bbbd1ff0868958c4
Seow Li Chong Glen <glenseowlc@hotmail.com>
2019-11-22 00:08:52 +0800

    Updates

---
+++ precache-manifest.95eabd38ac4a1418705ff02876e30024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307c926542dfa29da14b2a0e090fa8af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f1c86a720d9c004fd420",
+    "url": "/monsters-rolodex/static/css/main.04599c63.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "f1c86a720d9c004fd420",
+    "url": "/monsters-rolodex/static/js/main.d34944bc.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
